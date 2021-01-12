39d0b3f85e24f720a60641e7b4593aa64e6158b5
shr783789 <thomas783789@gmail.com>
2021-01-13 00:45:56 +0900

    bfs

--- baekjun/14501.py
+++ baekjun/14501.py
@@ -1,3 +1,4 @@
+# 퇴사
 from collections import defaultdict
 N = int(input())
 dic = defaultdict()
@@ -7,7 +8,10 @@ for _ in range(N) :
     days += 1
     dic[days] = list(map(int,input().split(' ')))
 answer = 0
-queue = [[1] + dic[1] + [0]] 
+if N >= 5 : 
+    queue = [list([i] + dic[i] + [0]) for i in range(1,6)] # 1~5일 중 시작 가능 5가지
+else : 
+    queue = [list([i] + dic[i] + [0]) for i in range(1,N+1)]
 while queue : 
     temp = queue.pop(0) # temp[0]은 현재 날짜, temp[1,2]는 주어진 시간과 보수, temp[3] 그날까지 총 보수
     for t in range(temp[1],temp[1]+5) : # 그날과 합쳐진 날부터 +5까지 가능한 날을 모두 넣어준다. 
