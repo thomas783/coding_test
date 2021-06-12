dda974ab2c747d5edcc18b356bfae59ee01748ba
shr783789 <thomas783789@gmail.com>
2021-06-13 04:56:09 +0900

    dp

---
+++ baekjoon/dp/2293.py
@@ -0,0 +1,11 @@
+n,k = map(int,input().split())
+coin = []
+for _ in range(n) : 
+    coin.append(int(input()))
+dp = [0 for i in range(k+1)]
+dp[0] = 1
+for c in coin : 
+    for i in range(1,k+1) : 
+        if i-c>=0 : 
+            dp[i] += dp[i-c]
+print(dp[k])
