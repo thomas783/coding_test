390f7e41eb3146a8e59b8c13eba122e61eb0a55d
shr783789 <thomas783789@gmail.com>
2022-01-03 19:36:53 +0900

    dfs

--- baekjoon/dfs/4574.py
+++ baekjoon/dfs/4574.py
@@ -1,24 +1,71 @@
 # 스도미노쿠
 
 import sys
+# 해당 위치에 넣은 숫자가 가능한지 확인하는 함수
+def check(y,x,n,table) :
+    # 같은 열 확인
+    for k in range(9) : 
+        if k != x and table[y][k] == n : 
+            return False
+    # 같은 행 확인
+    for k in range(9) : 
+        if k != y and table[k][x] == n : 
+            return False
+    # 같은 네모 확인
+    for i in range(y//3*3,y//3*3+3) : 
+        for j in range(x//3*3,x//3*3+3) : 
+            if table[i][j] == n and (i != y or j != x) : 
+                return False
+    return True
+
 def solve(can,table,empty) :
     if len(empty) == 0 : 
         for t in table : 
             print(''.join(map(str,t)))
-        sys.exit(0)
+        return
     y,x = empty[0]
     dy = [-1,0,0,1]
     dx = [0,-1,1,0] 
+    flag = False
     for d in range(4) : 
         ny = y + dy[d]
         nx = x + dx[d]
         if 0<=ny<9 and 0<=nx<9 and table[y][x] == 0 and table[ny][nx] == 0 :
-
-
+            for i in range(len(can)) : 
+                num1,num2 = can[i]
+                if check(y,x,num1,table) and check(ny,nx,num2,table) : 
+                    flag = True
+                    table[y][x], table[ny][nx] = num1,num2
+                    if len(can) > 1 : 
+                        can.remove([num1,num2])
+                        empty.remove([ny,nx])
+                        solve(can,table,empty[1:])
+                        can.insert(i,[num1,num2])
+                        empty.append([ny,nx])
+                        table[y][x],table[ny][nx] = 0,0
+                    else : 
+                        solve(can,table,[])
+                if check(y,x,num2,table) and check(ny,nx,num1,table) : 
+                    flag = True
+                    table[y][x], table[ny][nx] = num2,num1
+                    if len(can) > 1 :
+                        can.remove([num1,num2]) 
+                        empty.remove([ny,nx])
+                        solve(can,table,empty[1:])
+                        can.insert(i,[num1,num2])
+                        empty.append([ny,nx])
+                        table[y][x],table[ny][nx] = 0,0
+                    else : 
+                        solve(can,table,[])
+    if not flag : 
+        return
 input = sys.stdin.readline
 dic = dict(zip(['A','B','C','D','E','F','G','H','I'],range(9)))
-for th in range(2) : 
+th = 1
+while True : 
     N = int(input())
+    if N == 0 : 
+        break
     table = [[0 for _ in range(9)] for _ in range(9)]
     can = []
     for i in range(1,10) : 
@@ -39,6 +86,7 @@ for th in range(2) :
         for x in range(9) : 
             if table[y][x] == 0 : 
                 empty.append([y,x])
-    print('Puzzle ' + str(th+1))
+    print('Puzzle ' + str(th))
     solve(can,table,empty)
-dummy = input()
+    th += 1
+sys.exit(0)
